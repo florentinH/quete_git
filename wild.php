c3170cfd96994982aff798962bfd5d89431276e7
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-02-27 12:37:07 +0100

    A la Wild Code School on code en chaussette.

---
+++ wild.php
@@ -0,0 +1,24 @@
+
+    init
+
+Cette commande initialise le répertoire où elle est éxecutée comme un projet Git. Concrètement, cela va créer un répertoire ".git" contenant tout le nécessaire au bon fonctionnement de Git pour suivre toutes les modifications à l'intérieur de ce répertoire.
+
+    add
+
+Ajoute des fichiers à l'index de Git (stage ou cache sont des synonymes). À partir du moment ou un fichier est ajouté une fois dans l'index, il sera "tracké" par Git.
+
+    commit
+
+Enregistre l'index vers un nouveau commit avec un message décrivant les modifications effectuées (log) obligatoire.
+
+    status
+
+Affiche les fichiers ayant des différences entre index et HEAD, ceux ayant des différences entre le répertoire de travail et l'index ainsi que les fichiers non "trackés"
+
+    branch
+
+Lister, créer ou supprimer une branche.
+
+    checkout
+
+Permet de se déplacer d'une branche à l'autre (et bien d'autres choses)
